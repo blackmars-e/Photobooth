3c8cfae554e80d6e7908fe915531de622b481497
blackmars-e <marcel.boehnert@googlemail.com>
2024-09-30 10:26:16 +0200

    changes in preview

--- assets/js/preview.js
+++ assets/js/preview.js
@@ -12,15 +12,14 @@ const photoboothPreview = (function () {
             NONE: 'none',
             DEVICE: 'device_cam',
             URL: 'url',
-            ELGATO: 'elgato_cam'
+	    ELGATO: 'elgato_cam'
         },
-        // Removed `webcamConstraints` since it's not used in the code
-        elgatoConstraints = {
+        webcamConstraints = {
             audio: false,
             video: {
                 width: config.preview.videoWidth,
                 height: config.preview.videoHeight,
-                label: 'Cam Link'
+                facingMode: config.preview.camera_mode
             }
         },
         api = {};
@@ -41,19 +40,20 @@ const photoboothPreview = (function () {
         video.show();
     };
 
-    api.initializeMedia = async function (
+    api.initializeMedia = function (
         cb = () => {
-            // No-op: Placeholder for optional callback
+            return;
         },
         retry = 0
     ) {
         photoboothTools.console.logDev('Preview: Trying to initialize media...');
         if (
             !navigator.mediaDevices ||
-            config.preview.mode === PreviewMode.NONE ||
-            config.preview.mode === PreviewMode.URL
+            config.preview.mode === PreviewMode.NONE.valueOf() ||
+            config.preview.mode === PreviewMode.URL.valueOf()
         ) {
             photoboothTools.console.logDev('Preview: No preview from device cam or no webcam available!');
+
             return;
         }
         const getMedia =
@@ -64,39 +64,168 @@ const photoboothPreview = (function () {
 
         if (!getMedia) {
             photoboothTools.console.logDev('Preview: Could not get media!');
+
             return;
         }
 
-        if (config.preview.mode === PreviewMode.ELGATO && elgatoConstraints.video.label) {
-            const deviceId = await api.findDeviceByLabel(elgatoConstraints.video.label);
-            if (deviceId) {
-                elgatoConstraints.video.deviceId = { exact: deviceId };
-            }
-        }
+        getMedia
+            .call(navigator.mediaDevices, webcamConstraints)
+            .then(function (stream) {
+                photoboothTools.console.logDev('Preview: getMedia done!');
+                api.stream = stream;
+                video.get(0).srcObject = stream;
+                cb();
+            })
+            .catch(function (error) {
+                photoboothTools.console.log('ERROR: Preview: Could not get user media: ', error);
+                if (retry < retryGetMedia) {
+                    photoboothTools.console.logDev(
+                        'Preview: Retrying to get user media. Retry ' + retry + ' / ' + retryGetMedia
+                    );
+                    retry += 1;
+                    setTimeout(function () {
+                        api.initializeMedia(cb, retry);
+                    }, 1000);
+                } else {
+                    photoboothTools.console.logDev(
+                        'ERROR: Preview: Unable to get user media. Failed retries: ' + retry
+                    );
+                }
+            });
+    };
 
-        try {
-            const stream = await getMedia(elgatoConstraints);
-            photoboothTools.console.logDev('Preview: getMedia done!');
-            api.stream = stream;
-            video.get(0).srcObject = stream;
-            cb();
-        } catch (error) {
-            photoboothTools.console.log('ERROR: Preview: Could not get user media: ', error);
-            if (retry < retryGetMedia) {
-                photoboothTools.console.logDev(
-                    'Preview: Retrying to get user media. Retry ' + retry + ' / ' + retryGetMedia
-                );
-                retry += 1;
-                setTimeout(function () {
-                    api.initializeMedia(cb, retry);
-                }, 1000);
-            } else {
-                photoboothTools.console.logDev(
-                    'ERROR: Preview: Unable to get user media. Failed retries: ' + retry
-                );
-            }
+    api.getAndDisplayMedia = function (mode) {
+        if (api.stream && api.stream.active) {
+            api.changeVideoMode(mode);
+        } else {
+            api.initializeMedia(() => {
+                api.changeVideoMode(mode);
+            });
         }
     };
 
-    // Other functions remain the same...
+    api.runCmd = function (mode) {
+        const dataVideo = {
+            play: mode,
+            pid: pid
+        };
+
+        jQuery
+            .post('api/previewCamera.php', dataVideo)
+            .done(function (result) {
+                photoboothTools.console.log('Preview: ' + dataVideo.play + ' webcam successfully.');
+                pid = result.pid;
+            })
+            // eslint-disable-next-line no-unused-vars
+            .fail(function (xhr, status, result) {
+                photoboothTools.console.log('ERROR: Preview: Failed to ' + dataVideo.play + ' webcam!');
+            });
+    };
+
+    api.startVideo = function (mode, retry = 0, maxGetMediaRetry = 3) {
+        retryGetMedia = maxGetMediaRetry;
+        photoboothTools.console.log('Preview: startVideo mode: ' + mode);
+        if (config.preview.mode !== PreviewMode.URL.valueOf()) {
+            if (!navigator.mediaDevices || config.preview.mode === PreviewMode.NONE.valueOf()) {
+                return;
+            }
+        }
+
+        switch (mode) {
+            case CameraDisplayMode.INIT:
+                photoboothTools.console.logDev('Preview: Running preview cmd (INIT).');
+                api.runCmd('start');
+                break;
+            case CameraDisplayMode.BACKGROUND:
+                if (
+                    config.preview.mode === PreviewMode.DEVICE.valueOf() &&
+                    config.commands.preview &&
+                    !config.preview.bsm
+                ) {
+                    photoboothTools.console.logDev('Preview: Running preview cmd (BACKGROUND).');
+                    api.runCmd('start');
+                }
+                api.getAndDisplayMedia(CameraDisplayMode.BACKGROUND);
+                break;
+            case CameraDisplayMode.COUNTDOWN:
+                if (config.commands.preview) {
+                    if (
+                        config.preview.bsm ||
+                        (!config.preview.bsm && retry > 0) ||
+                        (typeof photoBooth !== 'undefined' && photoBooth.nextCollageNumber > 0)
+                    ) {
+                        photoboothTools.console.logDev('Preview: Running preview cmd (COUNTDOWN).');
+                        api.runCmd('start');
+                    }
+                }
+                if (config.preview.mode === PreviewMode.DEVICE.valueOf()) {
+                    photoboothTools.console.logDev('Preview: Preview at countdown from device cam.');
+                    api.getAndDisplayMedia(CameraDisplayMode.COUNTDOWN);
+                } else if (config.preview.mode === PreviewMode.URL.valueOf()) {
+                    photoboothTools.console.logDev('Preview: Preview at countdown from URL.');
+                    setTimeout(function () {
+                        url.show();
+                        url.addClass('streaming');
+                    }, config.preview.url_delay);
+                }
+                break;
+            case CameraDisplayMode.TEST:
+                if (config.preview.mode === PreviewMode.DEVICE.valueOf()) {
+                    photoboothTools.console.logDev('Preview: Preview from device cam.');
+                    api.getAndDisplayMedia(CameraDisplayMode.TEST);
+                } else if (config.preview.mode === PreviewMode.URL.valueOf()) {
+                    photoboothTools.console.logDev('Preview: Preview from URL.');
+                    setTimeout(function () {
+                        url.show();
+                        url.addClass('streaming');
+                    }, config.preview.url_delay);
+                }
+                break;
+            default:
+                photoboothTools.console.log('ERROR: Preview: Call for unexpected video mode: ' + mode);
+                break;
+        }
+    };
+
+    api.stopPreview = function () {
+        if (config.commands.preview_kill) {
+            api.runCmd('stop');
+        }
+        if (config.preview.mode === PreviewMode.DEVICE.valueOf()) {
+            api.stopVideo();
+        } else if (config.preview.mode === PreviewMode.URL.valueOf()) {
+            url.removeClass('streaming');
+            url.hide();
+        }
+    };
+
+    api.stopVideo = function () {
+        loader.css('--stage-background', null);
+        if (api.stream) {
+            api.stream.getTracks()[0].stop();
+            api.stream = null;
+        }
+        video.hide();
+        pictureFrame.hide();
+        collageFrame.hide();
+    };
+
+    api.setElements = () => {
+        video = $('#preview--video');
+        loader = $('.stage[data-stage="loader"]');
+        url = $('#preview--ipcam');
+        pictureFrame = $('#previewframe--picture');
+        collageFrame = $('#previewframe--collage');
+    };
+
+    api.init = function () {
+        api.setElements();
+    };
+
+    return api;
 })();
+
+$(function () {
+    photoboothPreview.init();
+    photoboothTools.console.log('Preview: Preview functions available.');
+});
